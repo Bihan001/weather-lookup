3aa07222e4e7e62dd0937a25af9db224b6dd1e4d
Bihan Chakraborty <bhchakraborty001@gmail.com>
2021-01-25 12:51:01 +0530

    [FIX] map bug

---
+++ config/webpack/environment.js
@@ -0,0 +1,1 @@
+environment.loaders.delete('nodeModules');
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import fetchWeather from '../api/fetchWeather';
 import ReactMapGL, { Marker, NavigationControl, LinearInterpolator } from 'react-map-gl';
-// @ts-ignore
+import mapboxgl from 'mapbox-gl'; // This is a dependency of react-map-gl even if you didn't explicitly install it
+
 // eslint-disable-next-line import/no-webpack-loader-syntax
-ReactMapGL.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
+mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
 
 const mapAPIKey = 'pk.eyJ1IjoiYmloYW5jIiwiYSI6ImNrazlmamhsMzAydDMyd254aWZ2dTYwaWIifQ.LqqynIf2WmW-2BFGZrnMnw';
 
